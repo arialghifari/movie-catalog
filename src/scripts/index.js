b2de15fdeb68e416a79d2f5c16721db6d9d2971e
arialghifari <arialghifari99@gmail.com>
2022-09-13 13:44:37 +0800

    Add navigation drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
 import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/responsive.css';
+import App from './views/app.js';
 
-console.log('Hello deck!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
